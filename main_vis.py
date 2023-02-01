1995a56037cae6ae1c17ad4a34f40d819ba1ee56
Dong-Hee Paek <donghee.paek@kaist.ac.kr>
2023-02-01 23:12:54 +0900

    Add files via upload

---
+++ main_vis.py
@@ -0,0 +1,12 @@
+'''
+* Copyright (c) AVELab, KAIST. All rights reserved.
+* author: Donghee Paek & Kevin Tirta Wijaya, AVELab, KAIST
+* e-mail: donghee.paek@kaist.ac.kr, kevin.tirta@kaist.ac.kr
+'''
+
+from uis.ui_vis import startUi
+
+if __name__ == '__main__':
+    path_cfg = './configs/cfg_GUI_TOOL.yml'
+    startUi(path_cfg)
+    
