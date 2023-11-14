174203457ab3694fba5ef7f72a798cca77d5e9b6
Dong-Hee Paek <donghee.paek@kaist.ac.kr>
2023-11-14 17:12:35 +0900

    Add files via upload

--- models/backbone_2d/__init__.py
+++ models/backbone_2d/__init__.py
@@ -1,7 +1,9 @@
 from .pillars_backbone import PillarsBackbone
 from .resnet_wrapper import ResNetFPN
+from .base_bev_backbone import BaseBEVBackbone
 
 __all__ = {
     'PillarsBackbone': PillarsBackbone,
     'ResNetFPN': ResNetFPN,
+    'BaseBEVBackbone': BaseBEVBackbone,
 }
---
+++ models/backbone_2d/map_to_bev/__init__.py
@@ -0,0 +1,5 @@
+from .height_compression import HeightCompression
+
+__all__ = {
+    'HeightCompression': HeightCompression,
+}
