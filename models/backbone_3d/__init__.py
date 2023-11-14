51e900c924c7cd9e3c2d3f45b40e9bcea3215f39
Dong-Hee Paek <donghee.paek@kaist.ac.kr>
2023-11-14 17:13:46 +0900

    Add files via upload

--- models/backbone_3d/__init__.py
+++ models/backbone_3d/__init__.py
@@ -1,7 +1,9 @@
 from .rdr_sp_pw import RadarSparseBackbone
 from .rdr_sp_dop import RadarSparseBackboneDop
+from .spconv_backbone import VoxelBackBone8x
 
 __all__ = {
     'RadarSparseBackbone': RadarSparseBackbone,
     'RadarSparseBackboneDop': RadarSparseBackboneDop,
+    'VoxelBackBone8x': VoxelBackBone8x,
 }
---
+++ models/backbone_3d/vfe/__init__.py
@@ -0,0 +1,5 @@
+from .mean_vfe import MeanVFE
+
+__all__ = {
+    'MeanVFE': MeanVFE
+}
