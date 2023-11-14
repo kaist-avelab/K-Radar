94e1dad061a6c9e64c8858f84eb77d470a35bcce
Dong-Hee Paek <donghee.paek@kaist.ac.kr>
2023-11-14 17:14:21 +0900

    Add files via upload

--- models/head/__init__.py
+++ models/head/__init__.py
@@ -8,10 +8,14 @@ from .rdr_spcube_head import RdrSpcubeHead
 from .ldr_pillars_head import LdrPillarsHead
 from .rdr_spcube_head_multi_cls import RdrSpcubeHeadMultiCls
 from .anchor_head import AnchorHeadSingle
+from .point_head_simple import PointHeadSimple
+from .center_head import CenterHead
 
 __all__ = {
     'RdrSpcubeHead': RdrSpcubeHead,
     'LdrPillarsHead': LdrPillarsHead,
     'RdrSpcubeHeadMultiCls': RdrSpcubeHeadMultiCls,
     'AnchorHeadSingle': AnchorHeadSingle,
+    'PointHeadSimple': PointHeadSimple,
+    'CenterHead': CenterHead,
 }
