b70b8250b59877cf71d4f046ee7049fe87f69e41
Dong-Hee Paek <dhe.paek@gmail.com>
2025-04-22 17:04:17 +0900

    Update __init__.py

--- models/backbone_3d/__init__.py
+++ models/backbone_3d/__init__.py
@@ -1,9 +1,11 @@
 from .rdr_sp_pw import RadarSparseBackbone
 from .rdr_sp_dop import RadarSparseBackboneDop
 from .spconv_backbone import VoxelBackBone8x
+from .rdr_sp_filt_backbone import RadarSparseFiltBackbone
 
 __all__ = {
     'RadarSparseBackbone': RadarSparseBackbone,
     'RadarSparseBackboneDop': RadarSparseBackboneDop,
     'VoxelBackBone8x': VoxelBackBone8x,
+    'RadarSparseFiltBackbone': RadarSparseFiltBackbone,
 }
