7398ebf372b73bc1c917110b645e96fb2c1006cc
Dong-Hee Paek <dhe.paek@gmail.com>
2025-04-22 17:03:03 +0900

    Update __init__.py

--- models/pre_processor/__init__.py
+++ models/pre_processor/__init__.py
@@ -1,7 +1,11 @@
 from .rdr_sparse_processor import RadarSparseProcessor
 from .rdr_sparse_processor_dop import RadarSparseProcessorDop
+from .rdr_polar_processor import RadarPolarProcessor
+from .rdr_filt_processor import RadarFiltProcessor
 
 __all__ = {
     'RadarSparseProcessor': RadarSparseProcessor,
     'RadarSparseProcessorDop': RadarSparseProcessorDop,
+    'RadarPolarProcessor': RadarPolarProcessor,
+    'RadarFiltProcessor': RadarFiltProcessor,
 }
