f2068a0723b6581ecfc1d77fe1b26ae9c27ec623
Don <dhe.paek@gmail.com>
2025-11-24 15:21:03 +0900

    Update __init__.py

--- models/backbone_2d/__init__.py
+++ models/backbone_2d/__init__.py
@@ -1,9 +1,13 @@
 from .pillars_backbone import PillarsBackbone
 from .resnet_wrapper import ResNetFPN
 from .base_bev_backbone import BaseBEVBackbone
+from .image_backbone import ImageBackbone
+from .generalized_lss import GeneralizedLSSFPN
 
 __all__ = {
     'PillarsBackbone': PillarsBackbone,
     'ResNetFPN': ResNetFPN,
     'BaseBEVBackbone': BaseBEVBackbone,
+    'ImageBackbone': ImageBackbone,
+    'GeneralizedLSSFPN': GeneralizedLSSFPN,
 }
