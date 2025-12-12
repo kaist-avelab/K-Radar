01618cf2c9f2f0a1f12758dc653547023a9af7ec
Don <dhe.paek@gmail.com>
2025-12-12 13:45:49 +0900

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,9 +1,11 @@
 from .kradar_detection_v1_0 import KRadarDetection_v1_0
 from .kradar_detection_v1_1 import KRadarDetection_v1_1
 from .kradar_detection_v2_0 import KRadarDetection_v2_0
+from .kradar_fusion_v1_0 import KRadarFusion_v1_0
 
 __all__ = {
     'KRadarDetection_v1_0': KRadarDetection_v1_0,
     'KRadarDetection_v1_1': KRadarDetection_v1_1,
     'KRadarDetection_v2_0': KRadarDetection_v2_0,
+    'KRadarFusion_v1_0': KRadarFusion_v1_0,
 }
